20c68cdd4d4deb81c7954bf73111a0bb09a64b19
Nozomi Tanetani <n.tanetani@gmail.com>
2025-04-15 18:17:09 +0900

    依存関係の修正

--- src/js/majiang.js
+++ src/js/majiang.js
@@ -1,5 +1,5 @@
 /*!
- *  電脳麻将 v2.4.16
+ *  Fever麻将 v2.4.16
  *
  *  Copyright(C) 2017 Satoshi Kobayashi
  *  Released under the MIT license
@@ -10,5 +10,5 @@ global.Majiang = require('@kobalab/majiang-core');
 global.Majiang.AI = require('@kobalab/majiang-ai');
 global.Majiang.UI = require('@kobalab/majiang-ui');
 global.Majiang.VERSION = '2.4.16';
-global.jQuery  = require('jquery');
+global.jQuery = require('jquery');
 global.$ = jQuery;
