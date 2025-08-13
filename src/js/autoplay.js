46faa573c26597837c4ec876e6c3ee89c4c1fc7d
Nozomi Tanetani <n.tanetani@gmail.com>
2025-08-14 06:15:54 +0900

    3p sync

--- src/js/autoplay.js
+++ src/js/autoplay.js
@@ -8,73 +8,73 @@
 "use strict";
 
 const { hide, show, fadeIn, scale,
-        setSelector, clearSelector  } = Majiang.UI.Util;
+    setSelector, clearSelector } = Majiang.UI.Util;
 
 let loaded;
 
-$(function(){
+$(function () {
 
     let game;
-    const pai   = Majiang.UI.pai($('#loaddata'));
+    const pai = Majiang.UI.pai($('#loaddata'));
     const audio = Majiang.UI.audio($('#loaddata'));
 
     const rule = Majiang.rule(
-                    JSON.parse(localStorage.getItem('Majiang.rule')||'{}'));
+        JSON.parse(localStorage.getItem('Majiang.rule') || '{}'));
 
     let open_shoupai = false;
-    let open_he      = false;
+    let open_he = false;
 
     function start() {
         if (game) {
             open_shoupai = game._view.open_shoupai;
-            open_he      = game._view.open_he;
+            open_he = game._view.open_he;
         }
         let players = [];
-        for (let i = 0; i < 4; i++) {
+        for (let i = 0; i < 3; i++) { // 3人麻雀
             players[i] = new Majiang.AI();
         }
         game = new Majiang.Game(players, start, rule);
         game.view = new Majiang.UI.Board($('#board .board'),
-                                        pai, audio, game.model);
-        game.wait  = 5000;
+            pai, audio, game.model);
+        game.wait = 5000;
         game._model.title
             = game._model.title.replace(/^[^\n]*/, $('title').text());
         game._view.open_shoupai = open_shoupai;
-        game._view.open_he      = open_he;
+        game._view.open_he = open_he;
 
-        $('body').attr('class','board');
+        $('body').attr('class', 'board');
         scale($('#board'), $('#space'));
 
-        $(window).off('keyup').on('keyup', (ev)=>{
+        $(window).off('keyup').on('keyup', (ev) => {
             if (ev.key == ' ') {
                 if (gamectl.stoped) gamectl.start();
-                else                gamectl.stop();
-                game.handler = ()=> gamectl.stop();
+                else gamectl.stop();
+                game.handler = () => gamectl.stop();
             }
             else if (ev.key == 's') gamectl.shoupai();
             else if (ev.key == 'h') gamectl.he();
             return false;
         });
-        $('#board .board').off('click').on('click', ()=>{
+        $('#board .board').off('click').on('click', () => {
             if (gamectl.stoped) gamectl.start();
-            else                gamectl.stop();
-            game.handler = ()=> gamectl.stop();
+            else gamectl.stop();
+            game.handler = () => gamectl.stop();
         });
         $('#board .board > .shoupai')
             .off('click', '.pai')
-            .on('click', '.pai', ()=>gamectl.shoupai());
+            .on('click', '.pai', () => gamectl.shoupai());
         $('#board .board > .he')
             .off('click', '.pai')
-            .on('click', '.pai', ()=>gamectl.he());
+            .on('click', '.pai', () => gamectl.he());
 
         const gamectl = new Majiang.UI.GameCtl($('#board'), 'Majiang.pref',
-                                                game, game._view);
+            game, game._view);
         game.kaiju();
     }
 
-    $(window).on('resize', ()=>scale($('#board'), $('#space')));
+    $(window).on('resize', () => scale($('#board'), $('#space')));
 
-    $(window).on('load', function(){
+    $(window).on('load', function () {
         hide($('#title .loading'));
         $('#title .start').on('click', start)
         show($('#title .start'));
@@ -82,4 +82,4 @@ $(function(){
     if (loaded) $(window).trigger('load');
 });
 
-$(window).on('load', ()=> loaded = true);
+$(window).on('load', () => loaded = true);
